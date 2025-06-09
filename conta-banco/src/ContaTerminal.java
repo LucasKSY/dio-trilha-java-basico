fa364e47d2afc616accee500707b8986ab0af608
Lucas Kenji <lucaskenji@suprasys.com.br>
2025-06-09 17:39:32 -0300

    feat: criar o terminal do projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new Scanner(System.in);
+        System.out.println("Insira o número de sua conta:");
+        int numero = Integer.valueOf(scanner.nextLine());
+        System.out.println("Insira o número de sua agência com o dígito (xxx-x):");
+        String agencia = scanner.nextLine();
+        System.out.println("Insira seu nome completo:");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Insira o saldo de sua conta:");
+        Double saldo;
+        String saldoString = scanner.nextLine();
+        try {
+            saldo = Double.valueOf(saldoString);
+        } catch (NumberFormatException exception) {
+            saldo = Double.valueOf(saldoString.replace(".", "").replace(",", "."));
+        }
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque \n",
+                nomeCliente, agencia, numero, saldo);
+        scanner.close();
     }
 }
